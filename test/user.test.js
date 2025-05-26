2cb97e932d4674a487d5a1f1b239643efef656ff
Sarmad Faizan <sarmadf49@gmail.com>
2025-05-27 00:01:22 +0500

    add snapshots command

--- test/user.test.js
+++ test/user.test.js
@@ -1,6 +1,9 @@
 import {describe, test} from 'node:test'
 import { fetchData } from '../app.js'
 
+
+// node --test --test-update-snapshots
+
 describe('User Fetched',()=>{
     test('fetches data from server', (t)=>{
         const data = fetchData(1)
